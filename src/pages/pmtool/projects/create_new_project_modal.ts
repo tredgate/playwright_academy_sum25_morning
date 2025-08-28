473f2c1dbd35e9a2a34ce5f7931ec14a49563f9c
Petr Fifka <petrfifk@gmail.com>
2025-08-28 11:04:41 +0200

    atomic tests - page objects, tests

--- src/pages/pmtool/projects/create_new_project_modal.ts
+++ src/pages/pmtool/projects/create_new_project_modal.ts
@@ -1,15 +1,74 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 import { ProjectTasksPage } from "./project_tasks_page.ts";
+import { ProjectsPage } from "../projects_page.ts";
 
 export class CreateNewProjectModal {
   readonly page: Page;
   readonly saveButton: Locator;
   readonly nameInput: Locator;
+  readonly titleHeader: Locator;
+  readonly infoTab: Locator;
+  readonly prioritySelect: Locator;
+  readonly priorityLabel: Locator;
+  readonly statusSelect: Locator;
+  readonly statusLabel: Locator;
+  readonly nameLabel: Locator;
+  readonly nameValidationDiv: Locator;
+  readonly startDateInput: Locator;
+  readonly startDateLabel: Locator;
+  readonly descriptionFrame: string;
+  readonly descriptionLabel: Locator;
+  readonly attachmentsButton: Locator;
+  readonly attachmentsLabel: Locator;
+  readonly closeButton: Locator;
+  readonly alertMessageDiv: Locator;
+  readonly deleteAttachmentButton: Locator;
+  readonly firstAttachmentName: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.nameInput = page.locator('//div[@data-testid="Name"]/input');
     this.saveButton = page.locator('//button[@type="submit"]');
+    this.titleHeader = page.locator(".modal-title");
+    this.infoTab = page.locator('//ul[@id="form_tabs"]//li[1]');
+    this.prioritySelect = page.locator(
+      '//div[@data-testid="Priority"]//select'
+    );
+    this.priorityLabel = page.locator(
+      '//div[@data-testid="Priority"]//ancestor::div[contains(@class, "form-group")]/label'
+    );
+    this.statusSelect = page.locator('//div[@data-testid="Status"]//select');
+    this.statusLabel = page.locator(
+      '//div[@data-testid="Status"]//ancestor::div[contains(@class, "form-group")]/label'
+    );
+    this.nameLabel = page.locator(
+      '//div[@data-testid="Name"]//ancestor::div[contains(@class, "form-group")]/label'
+    );
+    this.nameValidationDiv = page.locator('[data-testid="Name"] label.error');
+    this.startDateInput = page.locator(
+      '//div[@data-testid="Start Date"]//input'
+    );
+    this.startDateLabel = page.locator(
+      '//div[@data-testid="Start Date"]//ancestor::div[contains(@class, "form-group")]/label'
+    );
+    this.descriptionFrame = '//div[@data-testid="Description"]//iframe';
+    this.descriptionLabel = page.locator(
+      '//div[@data-testid="Description"]//ancestor::div[contains(@class, "form-group")]/label'
+    );
+    this.attachmentsButton = page.locator(
+      '//div[@data-testid="Attachments"]//div[contains(@class, "btn-upload")]'
+    );
+    this.attachmentsLabel = page.locator(
+      '//div[@data-testid="Attachments"]//ancestor::div[contains(@class, "form-group")]/label'
+    );
+    this.closeButton = page.locator(".btn-close");
+    this.alertMessageDiv = page.locator("#form-error-container .alert");
+    this.deleteAttachmentButton = page.locator(
+      '//div[@data-testid="Attachments"]//label[contains(@class, "delete_attachments")]'
+    );
+    this.firstAttachmentName = page.locator(
+      '//div[@data-testid="Attachments"]//div[contains(@class, "attachments-form-list")]//tr[1]/td[1]'
+    );
   }
 
   async fillName(projectName: string): Promise<this> {
@@ -21,16 +80,50 @@ export class CreateNewProjectModal {
     await this.saveButton.click();
     return new ProjectTasksPage(this.page);
   }
-}
 
-/*
-ProjectTasksPage
-⌛5:00
-Prvky i metody zkopírujeme z DashboardPage
-Prvek:
-profileButton
-logoutButton
-Metoda:
-clickProfile(): ProjectTasksPage
-clickLogout(): LoginPage
-*/
+  async clickClose(): Promise<ProjectsPage> {
+    await this.closeButton.click();
+    return new ProjectsPage(this.page);
+  }
+
+  async selectPriority(priorityValue: string): Promise<this> {
+    await this.prioritySelect.selectOption(priorityValue);
+    return this;
+  }
+
+  async selectStatus(statusValue: string): Promise<this> {
+    await this.statusSelect.selectOption(statusValue);
+    return this;
+  }
+
+  async fillStartDate(startDate: string): Promise<this> {
+    await this.startDateInput.fill(startDate);
+    return this;
+  }
+
+  async fillDescription(description: string): Promise<this> {
+    const iframe = this.page.frameLocator(this.descriptionFrame);
+    await iframe.locator("body").fill(description);
+    return this;
+  }
+
+  async uploadFile(filePath: string): Promise<this> {
+    const fileChooserPromise = this.page.waitForEvent("filechooser");
+    await this.attachmentsButton.click();
+    const fileInput = await fileChooserPromise;
+    await fileInput.setFiles(filePath);
+    await expect(this.deleteAttachmentButton).toBeVisible();
+    return this;
+  }
+
+  async triggerNameInputValidation(): Promise<this> {
+    await this.nameInput.clear();
+    await this.saveButton.click();
+    return this;
+  }
+
+  async triggerAlarmMessage(): Promise<this> {
+    await this.triggerNameInputValidation();
+    return this;
+  }
+}
--- tests/learning/fluent-interface/fluent_api_login_tests.spec.ts
+++ tests/learning/fluent-interface/fluent_api_login_tests.spec.ts
@@ -14,22 +14,3 @@ test("Fluent Interface - Login Test", async ({ page }) => {
     .then((dashboard) => dashboard.clickProfile())
     .then((dashboard) => dashboard.clickLogout());
 });
-/*
-Cvičení - vytvoření testu na ztrátu hesla pomocí Fluent API (⌛15:00)
-Vytvořte nový test na ztrátu hesla (tests/exercises/lost_password_tests.spec.ts)
-Vytvořte nový Page Object pro stránku ztráta hesla (lost_password_page.ts) ve složce: src/pages/pmtool
-Vytvořte akce v novém objektu:
-Vyplnění username: typeUsername(), 
-lokátor: [name="username"]
-Vyplnění e-mailu: typeEmail(), 
-lokátor: [name="email"] 
-Kliknutí na Send: clickSend(), 
-lokátor: [type="submit"]
-Kliknutí na tlačítko zpět: clickBack(), 
-lokátor: #back-btn
-Upravte Page Object LoginPage, vložte kliknutí na Lost Password:  clickPasswordForgotten(), lokátor: #forget_password 
-Vytvořte testy:
-Ztracené heslo end to end (username: lost_password_user, mail: lost_password@tredgate.cz)
-Otevření stránky ztraceného hesla, návrat na login.
-
-*/
